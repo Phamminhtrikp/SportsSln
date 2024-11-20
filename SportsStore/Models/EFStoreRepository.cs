481b4c7627904c00356d112cd3365660d1cd52e0
Phamminhtrikp <tripham.210504@gmail.com>
2024-11-20 21:45:24 +0700

    From test branch: add crud method

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -8,5 +8,20 @@
             context = ctx;
         }
         public IQueryable<Product> Products => context.Products;
+
+        public void SaveProduct(Product product)
+        {
+            context.SaveChanges();
+        }
+        public void DeleteProduct(Product product)
+        {
+            context.Remove(product);
+            context.SaveChanges();
+        }
+        public void CreateProduct(Product product)
+        {
+            context.Add(product);
+            context.SaveChanges();
+        }
     }
 }
